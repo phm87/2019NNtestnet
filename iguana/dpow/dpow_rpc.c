c29d648526f2c550413c2e0f9914092511b2712b
phm87 <31578435+phm87@users.noreply.github.com>
2019-08-22 01:02:36 +0200

    Possible fix for BTC

--- iguana/dpow/dpow_rpc.c
+++ iguana/dpow/dpow_rpc.c
@@ -609,8 +609,12 @@ cJSON *dpow_listunspent(struct supernet_info *myinfo,struct iguana_info *coin,ch
     if ( coin->active == 0 ) return (0);
     if ( coin->FULLNODE < 0 )
     {
-	if ( strcmp(coin->symbol,"BTC") == 0 || strcmp(coin->symbol,"KMD") == 0 )	{
+	if ( strcmp(coin->symbol,"KMD") == 0 )	{
             sprintf(buf,"10000, \"%s\"",coinaddr);
+	    sprintf(call,"dpowlistunspent");
+	}
+	else if ( strcmp(coin->symbol,"BTC") == 0 )	{
+            sprintf(buf,"1, 99999999, [\"%s\"]",coinaddr);
 	    sprintf(call,"dpowlistunspent");
 	}
 	else	{
