0ca8b68adf8a750e2edcce9f6fd316f345ab08d6
blackjok3r <blackjok3r@gmail.com>
2019-08-21 17:26:55 +0800

    :facepalm:

--- iguana/iguana_notary.c
+++ iguana/iguana_notary.c
@@ -985,7 +985,7 @@ STRING_ARG(dpow,active,maskhex)
     n = komodo_notaries("KMD",pubkeys,current);
     if ( maskhex == 0 || maskhex[0] == 0 )
     {
-        if ( myinfo->DPOWS[0] == 0 || myinfo->DPOWS[0]->currentbp == 0 )
+        if ( myinfo->DPOWS[0] == 0 && myinfo->DPOWS[0]->currentbp == 0 )
             return(clonestr("{\"error\":\"there is no dpow round yet started to check.\"}"));
         return(jprint(dpow_recvmasks(myinfo,myinfo->DPOWS[0]->currentbp),1));
 
